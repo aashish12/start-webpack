512446eb5e73df9dc30476e83f045a3efe822897
aashish12 <aashishchapagain@gmail.com>
2017-04-29 20:33:36 +0545

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecreteState);
+updateSecreteParagraph();
+
+function toggleSecreteState(){
+	showSecret = !showSecret;
+	updateSecreteButton();
+	updateSecreteParagraph();
+}
+
+function updateSecreteButton(){
+	if(showSecret){
+		secretButton.textContent = 'Hide the Secrete';
+	}else{
+		secretButton.textContent = 'Show the Secrete';
+	}
+}
+
+function updateSecreteParagraph(){
+	if(showSecret){
+		secretParagraph.style.display = 'block';
+	}else{
+		secretParagraph.style.display = 'none';
+	}
+}
